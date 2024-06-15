2748b45a0134ab466445ee9d7e3a4e80c20404c2
BakangMonei <bakangmonei2@gmail.com>
2024-06-16 00:43:57 +0200

    Changing to js and jsx

--- app/index.tsx
+++ app/index.tsx
@@ -3,11 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 
-import SplashScreen from "../src/layouts/SplashScreen";
-import LoginScreen from "../src/screens/Auth/LoginScreen";
-import UserIndex from "../src/screens/User/UserIndex";
-import DashboardScreen from "../src/screens/SuperAdmin/DashboardScreen";
-
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 const Stack = createStackNavigator();
@@ -19,30 +14,6 @@ const theme = createTheme({
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Splash">
-          <Stack.Screen
-            name="Splash"
-            component={SplashScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Login"
-            component={LoginScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="UserIndex"
-            component={UserIndex}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Dashboard"
-            component={DashboardScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
     </ThemeProvider>
   );
 };
