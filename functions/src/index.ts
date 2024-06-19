1326277b788af11c3db59f3f60b1839c5ea390a9
BakangMonei <bakangmonei2@gmail.com>
2024-06-19 19:42:08 +0200

    deployed

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,13 +17,16 @@ export const sendEmailNotification = functions.firestore
   .onCreate(async (snapshot, context) => {
     const data = snapshot.data();
     const mailOptions = {
-      from: "bakangmonei2@gmail.com",
+      from: "BHC Application - Monei Bakang [Testing]",
       to: data.email,
       subject: "Application Received",
-      text: `Hello ${data.name},\n
-      \nYour application for the 
-      property ${data.propertyId} 
-      has been received. 
+      text: `Hello ${data.name},
+      \nYour application for the property ${data.propertyId} has been received.
+      
+      Phone: ${data.phone}
+      Message: ${data.message}
+      Property ID: ${data.propertyId}
+      Status: ${data.status}
       We will get back to you soon.
       \n\nBest regards,
       \nThe BHC Team`,
