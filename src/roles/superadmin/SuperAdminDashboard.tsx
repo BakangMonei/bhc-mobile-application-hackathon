c77d5a416f26766a9dafe90bdd803230f48306c0
BakangMonei <bakangmonei2@gmail.com>
2024-06-24 05:55:26 +0200

    gerg

--- src/roles/superadmin/SuperAdminDashboard.tsx
+++ src/roles/superadmin/SuperAdminDashboard.tsx
@@ -6,17 +6,20 @@ import SuperAdminUserManagementScreen from "./SuperAdminUserManagementScreen";
 import SuperAdminSecurityManagementScreen from "./SuperAdminSecurityManagementScreen";
 import SuperAdminMaintenanceOversightScreen from "./SuperAdminMaintenanceOversightScreen";
 import CustomDrawerContent from "../../components/drawers/CustomDrawerContent";
+import SuperAdminHomeScreen from "./SuperAdminHomeScreen";
 
 const Drawer = createDrawerNavigator();
 
 const SuperAdminDashboard = () => {
   return (
     <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
+       <Drawer.Screen name="SuperAdminHomeScreen" component={SuperAdminHomeScreen} />
       <Drawer.Screen name="User Management" component={SuperAdminUserManagementScreen} />
       <Drawer.Screen name="Content Management" component={SuperAdminContentManagementScreen} />
       <Drawer.Screen name="Analytics Reporting" component={SuperAdminAnalyticsScreen} />
       <Drawer.Screen name="Security Management" component={SuperAdminSecurityManagementScreen} />
       <Drawer.Screen name="Maintenance Oversight" component={SuperAdminMaintenanceOversightScreen} />
+      
     </Drawer.Navigator>
   );
 };
